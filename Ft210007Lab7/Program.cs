97e4234901802688ca83f9636c1d574681cbbdfc
YWKBM <ywkbm1973@gmail.com>
2022-11-09 16:48:24 +0500

    Добавленно: логгер, обработка ошибок.

--- Ft210007Lab7/Program.cs
+++ Ft210007Lab7/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,94 +12,133 @@ namespace Ft210007Lab7
     {
         static void Main(string[] args)
         {
-            while (true){ 
-            Board board = new Board();
-            Moves moves = new Moves(board);
+            StreamWriter logger = new StreamWriter("Logger.txt");
+
+            while (true){
+                Board board = new Board();
+                logger.WriteLine("Board initialized");
+                Moves moves = new Moves(board);
+                logger.WriteLine("Board initialized");
 
                 //инициализация координаты фигуры
-            Console.WriteLine("Enter the x pozition of figure: ");
-            int x = int.Parse(Console.ReadLine());
-            Console.WriteLine("Enter the y pozition of figure: ");
-            int y = int.Parse(Console.ReadLine());
+                Console.WriteLine("Enter the x pozition of figure [0 : 7]: ");
+                int x = int.Parse(Console.ReadLine());
+                logger.WriteLine("User added coordinate x: " + x);
+                Console.WriteLine("Enter the y pozition of figure [0 : 7]: ");
+                int y = int.Parse(Console.ReadLine());
+                logger.WriteLine("User added coordinate y: " + y);
 
-            if(x < 0 || y < 0 || x >= 8 || y >=8)//обработка ошибок ввода
-            { 
+                if (x < 0 || y < 0 || x >= 8 || y >=8)//обработка ошибок ввода
+                { 
                     Console.WriteLine("Board doesn't include this coordinates, try again");
-                    break;
-            }
-
-            //инициализаци фигуры
-            Console.WriteLine("Enter the name of figure (Q,B,R,N): ");
-            string inputName = Console.ReadLine();
-
-            Figure figure = (Figure)inputName[0];
-
-            if (((char)figure != (char)Figure.Qeen && (char)figure != (char)Figure.Knight && (char)figure != (char)Figure.Rook && (char)figure != (char)Figure.Bishop) || (char)figure == (char)Figure.Target || (char)figure == (char)Figure.Pozition)
-            {
-                Console.WriteLine("Wrong figure, try again");
-                    break;
-            }
-
-            //инициализация цели
-            Console.WriteLine("Enter the x pozition of target: ");
-            int xT = int.Parse(Console.ReadLine());
-            Console.WriteLine("Enter the y pozition of target: ");
-            int yT = int.Parse(Console.ReadLine());
-            if(xT < 0 || yT < 0 || xT >= 8 || yT >=8) //обработка ошибок ввода
-            { 
-                    Console.WriteLine("Board doesn't include this coordinates, try again");
-                    break;
-            }
-
-            board.SetFigureAt(x, y, figure);
-            board.SetFigureAt(xT, yT, Figure.Target);
-                   
-       
-            //вывод доски
-            Console.WriteLine(BoardToAcsii(board));
-
-            //формирование массива доступных ходов 
-            List<Square> allowedSquare = moves.FindAllMoves(board.fs[x, y]);
-
-            
-            //проверка возможности достижения цели за один ход
-            bool canMove = moves.CanMove(new FigureMoving(board.GetFigureAt(x, y), board.fs[x, y].square, board.fs[xT, yT].square));
-            Console.WriteLine("Turn to reach the target: " + canMove);
-
-            //формирования массива доступных ходов заданной фигуры с позиции цели
-            if (!canMove) { 
-            board.SetFigureAt(xT, yT, figure);
-            List<Square> AllowedMovesFromTarget = moves.FindAllMoves(board.fs[xT, yT]);
-            board.SetFigureAt(xT, yT, Figure.Target);
-
-            //Поиск пересечений
-
-            Square secondMove = Square.none;
-            for (int i = 0; i < AllowedMovesFromTarget.Count; i++)
-            {
-                if (allowedSquare.Contains(AllowedMovesFromTarget[i]))
-                {
-                    secondMove = AllowedMovesFromTarget[i];
+                    logger.WriteLine("User entered wrong coordinates: " + x + ";" + y); 
                     break;
                 }
-            }
-            Console.WriteLine();
-           
-            if (secondMove != Square.none)
-                 Console.WriteLine("Target can be reached by second move from X pozition with coordinates: " + secondMove.x + ";" + secondMove.y);
-            else
-            {
-                Console.WriteLine("Target can't be reached by second move");
-            }
 
-            board.SetFigureAt(secondMove.x, secondMove.y, Figure.Pozition);
-            Console.WriteLine(BoardToAcsii(board));
-            }
-            }
-            
-            
+            //инициализаци фигуры
+                Console.WriteLine("Enter the name of figure (Q,B,R,N): ");
+                string inputName = Console.ReadLine();
 
+                Figure figure = (Figure)inputName[0];
+
+                if (((char)figure != (char)Figure.Qeen && (char)figure != (char)Figure.Knight && (char)figure != (char)Figure.Rook && (char)figure != (char)Figure.Bishop) || (char)figure == (char)Figure.Target || (char)figure == (char)Figure.Pozition)
+                {
+                    Console.WriteLine("Wrong figure, try again");
+                    logger.WriteLine("User entered wrong figure name: " + (char)figure);
+                    break;
+                }
+
+            //инициализация цели
+                Console.WriteLine("Enter the x pozition of target: ");
+                int xT = int.Parse(Console.ReadLine());
+                logger.WriteLine("User added coordinate x: " + xT);
+                Console.WriteLine("Enter the y pozition of target: ");
+                int yT = int.Parse(Console.ReadLine());
+                logger.WriteLine("User added coordinate y: " + xT);
+                if (xT < 0 || yT < 0 || xT >= 8 || yT >=8) //обработка ошибок ввода
+                { 
+                    Console.WriteLine("Board doesn't include this coordinates, try again");
+                    logger.WriteLine("User entered wrong coordinates: " + xT + ";" + yT);
+                    break;
+                }
+
+                board.SetFigureAt(x, y, figure);
+                logger.WriteLine("Figure " + (char)figure + " setted on " + x + ";" + y);
+                board.SetFigureAt(xT, yT, Figure.Target);
+                logger.WriteLine("Figure " + (char)Figure.Target + " setted on " + xT + ";" + yT);
+
+
+
+                //вывод доски
+                Console.WriteLine(BoardToAcsii(board));
+
+                //проверка возможности достижения цели за один ход
+                bool canMove = moves.CanMove(new FigureMoving(board.GetFigureAt(x, y), board.fs[x, y].square, board.fs[xT, yT].square));
+                Console.WriteLine("Turn to reach the target: " + canMove);
+                if (!canMove)
+                {
+                    //формирование массива доступных ходов 
+                    List<Square> allowedSquare = moves.FindAllMoves(board.fs[x, y]);
+                    logger.WriteLine("Array of figures moves formation");
+                    foreach (Square square in allowedSquare)
+                    {
+                    logger.WriteLine(square.x + ";" + square.y);   
+                    }
+
+
+                    //формирования массива доступных ходов заданной фигуры с позиции цели
+                                       
+                    board.SetFigureAt(xT, yT, figure);
+                    logger.WriteLine("Figure " + (char)figure + " setted on " + xT + ";" + yT);
+                    logger.WriteLine("Array of figures moves formation");
+                    List<Square> AllowedMovesFromTarget = moves.FindAllMoves(board.fs[xT, yT]);
+                    foreach(Square allowedMove in AllowedMovesFromTarget)
+                    {
+                        logger.WriteLine(allowedMove.x + ";" + allowedMove.y);  
+                    }
+                    board.SetFigureAt(xT, yT, Figure.Target);
+                    logger.WriteLine("Figure " + (char)Figure.Target + " setted on " + xT + ";" + yT);
+
+
+
+                    //Поиск пересечений
+                    logger.WriteLine("Searching for intersections");
+                    Square secondMove = Square.none;
+                    for (int i = 0; i < AllowedMovesFromTarget.Count; i++)
+                    {
+                        if (allowedSquare.Contains(AllowedMovesFromTarget[i]))
+                        {
+                        secondMove = AllowedMovesFromTarget[i];
+                        break;
+                        }
+                    }
+                    Console.WriteLine();
+
+                    if (secondMove != Square.none) 
+                    {
+                        logger.WriteLine("Intersection has been found: " + secondMove.x + ";" + secondMove.y);
+                        Console.WriteLine("Target can be reached by second move from X pozition with coordinates: " + secondMove.x + ";" + secondMove.y);
+                    }
+
+
+                    else
+                    {
+                        Console.WriteLine("Target can't be reached by second move");
+                        logger.WriteLine("Intersection hasn't been found");
+                    }
+
+                    board.SetFigureAt(secondMove.x, secondMove.y, Figure.Pozition);
+                    logger.WriteLine("Figure " + (char)Figure.Pozition + " setted on " + secondMove.x + ";" + secondMove.y);
+                    Console.WriteLine(BoardToAcsii(board));
+                    break;
+            }
         }
+        
+            logger.Close();
+            
+            
+
+    }
 
         public static string BoardToAcsii(Board board)
         {
